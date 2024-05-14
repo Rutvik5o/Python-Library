04261bc4b94573a87063c2b0ca716a61bbfe5fda
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-05-14 20:06:50 +0530

    NumPy: Single & Multi Dimensional Array

---
+++ first.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+n1=np.array([10,20,30,40])  #single dimensional
+
+print(n1)
+
+#multi-dimensional
+
+n2=np.array([[19,28,34],[53,63,63]])
+
+print(n2)
+
+print(type(n2))
+
+
+
