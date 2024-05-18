fbf3e77979c6427af99080d06425425906dbba54
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-05-18 20:47:36 +0530

    NumPy : Mathematics

---
+++ mathematics.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+n1=np.array([10,20,30])
+
+n1=n1+1 #=> Addition
+
+print(n1)
+
+n2=n1*2 #=> Multiplication
+
+print(n2)
+
+n3=n1-1 #=> Subtraction
+
+print(n3)
+
+n4=n1/2 #=> Divison
+
+print(n4)
+
+print("Mean,Median,Standard Deviation")
+
+n2=np.array([3,64,46,366,6])
+
+print(np.mean(n2)) #=>Mean
+
+print(np.std(n2)) #=> Standard Deviation    
+
+print(np.median(n2)) #=>Median
